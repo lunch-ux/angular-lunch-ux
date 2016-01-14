d904f41166b0567c441e28c39d805e79a830ab49
Randi Abe <randi.abe@gmail.com>
2016-01-13 19:40:56 -0800

    Generated angular app with yeoman and installed initial bower dependencies

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lunchUxApp
+ * @description
+ * # lunchUxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lunchUxApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lunchUxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lunchUxApp
+ */
+angular.module('lunchUxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
